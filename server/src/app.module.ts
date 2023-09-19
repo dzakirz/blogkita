20dcf0d86a36d823c4373a7be1c0b4ff67a47c21
Dzaki Rozaan <dzakirz.dev@gmail.com>
2023-09-19 21:04:37 +0700

    wip(auth): create authentication and athorization

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
